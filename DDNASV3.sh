5b4095c9c7926ba87335f81b39cb01cb42b07806
adam.huang <sniperpr@gmail.com>
2012-06-14 21:17:33 +0800

    modify DDNASV3.sh

--- DDNASV3.sh
+++ DDNASV3.sh
@@ -2,9 +2,11 @@
 PWD=`pwd`
 CONFIG_DIR="$PWD"/DDNASCONFIG
 
-echo Now,mv DDNASV_defconfig.config to .config
+echo Now,mv DDNAS_defconfig.config to .config
+cp $CONFIG_DIR/DDNAS_defconfig.config $PWD
+echo mkdir dl,and cp linux-2.6.31.8.tar.bz2 to dl
+cp $PWD/linux-2.6.31.8.tar.bz2 $PWD/dl/
 
-cp $CONFIG_DIR/DDNASV_defconfig.config $PWD
 make V=99
 echo make V=99, first time, dot use make V=99 -jx
 
