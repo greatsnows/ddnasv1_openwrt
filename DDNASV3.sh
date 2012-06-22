cb9f7ac598b4392159d5c29705d77f69e17c8b5e
adam.huang <sniperpr@gmail.com>
2012-06-22 12:15:59 +0800

    fix DDNASV3.sh bug

--- DDNASV3.sh
+++ DDNASV3.sh
@@ -5,7 +5,8 @@ CONFIG_DIR="$PWD"/DDNASCONFIG
 echo Now,mv DDNAS_defconfig.config to .config
 cp $CONFIG_DIR/DDNAS_defconfig.config $PWD
 echo mkdir dl,and cp linux-2.6.31.8.tar.bz2 to dl
-cp $PWD/linux-2.6.31.8.tar.bz2 $PWD/dl/
+mkdir $PWD/dl
+cp $PWD/DL/linux-2.6.31.8.tar.bz2 $PWD/dl/
 
 make V=99
 echo make V=99, first time, dot use make V=99 -jx
